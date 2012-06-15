819621a36d08f9c75146443d5249fe8f6b8941a7
Andreas Presthammer <andhaug@gmail.com>
2012-06-15 22:17:40 +0200

    Added commandline argument support and refactor
    Also did extensive refactoring

--- HanselDownloader/Program.cs
+++ HanselDownloader/Program.cs
@@ -3,9 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Net.Http;
-using System.Text;
 using System.Text.RegularExpressions;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -13,34 +11,46 @@ namespace HanselDownloader
 {
 	class Program
 	{
+		private static readonly XNamespace FeedBurnerNamespace = @"http://rssnamespace.org/feedburner/ext/1.0";
 		private static readonly Uri Mp3Feed = new Uri(@"http://feeds.feedburner.com/HanselminutesCompleteMP3");
+		private static readonly Regex ShowIdRegex;
+		private static int _minShowId;
+		private static int _maxShowId;
+		private static DirectoryInfo _saveDirectory;
+		private const int MaxSimultaneousDownloads = 4;
+
+		static Program()
+		{
+			ShowIdRegex = new Regex(@"_(\d+)");
+		}
 
 		static void Main(string[] args)
 		{
-			
-			//Task<string> stringAsync = client.GetStringAsync(Mp3Feed);
+			try
+			{
+				SetupArgs(args);
 
-			var xDocument = XDocument.Load(Mp3Feed.AbsoluteUri);
-			//var xDocument = XDocument.Load(@"c:\temp\feed.xml");
+				var allShows = GetShows();
 
-			XNamespace feedBurner = "http://rssnamespace.org/feedburner/ext/1.0";
+				var showsToDownload = (from show in allShows
+				                       where
+				                       	show.Id >= _minShowId &&
+				                       	show.Id <= _maxShowId
+				                       select show).ToList();
 
-			var showIdRegex = new Regex(@"_(\d+)");
-				
-			var shows = from item in xDocument.Descendants("item")
-						let mp3LinkElem = item.Descendants(feedBurner + "origEnclosureLink").Single()
-						select new
-						{
-							Mp3Uri =  new Uri(mp3LinkElem.Value),
-							Id = int.Parse(showIdRegex.Matches(mp3LinkElem.Value)[0].Groups[1].ToString())
-				        };
+				DownloadInParallel(showsToDownload);
 
-			var showsToDownload = shows.Where(x => x.Id <= 100);
+				Console.WriteLine(string.Format("Finished downloading {0} shows to {1}", showsToDownload.Count(), _saveDirectory));
+			}
+			catch (Exception e)
+			{
+				Console.Error.WriteLine(e);
+			}
+		}
 
-			ParallelOptions po = new ParallelOptions
-			                     	{
-			                     		MaxDegreeOfParallelism = 5
-			                     	};
+		private static void DownloadInParallel(IEnumerable<Show> showsToDownload)
+		{
+			var po = new ParallelOptions {MaxDegreeOfParallelism = MaxSimultaneousDownloads};
 			Parallel.ForEach(showsToDownload, po, show =>
 				{
 					try
@@ -49,15 +59,15 @@ namespace HanselDownloader
 						{
 							var downloadStream = httpClient.GetStreamAsync(show.Mp3Uri).Result;
 
-							var fileLocation = string.Format(@"c:\temp\shows\Hanselminutes_{0}.mp3", show.Id);
+							var file = new FileInfo(_saveDirectory + string.Format(@"\Hanselminutes_{0}.mp3", show.Id));
 							using (downloadStream)
-							using (var fileStream = File.Create(fileLocation))
+							using (var fileStream = file.Create())
 							{
 								Console.WriteLine(string.Format("Downloading show {0}", show.Id));
 								downloadStream.CopyTo(fileStream);
 							}
 
-							Console.WriteLine(string.Format("Show {0} downloaded to {1}", show.Id, fileLocation));
+							Console.WriteLine(string.Format("Show {0} downloaded to {1}", show.Id, file));
 						}
 					}
 					catch (Exception e)
@@ -65,9 +75,40 @@ namespace HanselDownloader
 						Console.Error.WriteLine(e);
 					}
 				});
+		}
 
-			Console.WriteLine("Done......");
-			Console.ReadKey();
+		private static void SetupArgs(string[] args)
+		{
+			if (!int.TryParse(args.ElementAtOrDefault(0), out _minShowId))
+				throw new ArgumentException("First argument is the minimum show id");
+
+			if (!int.TryParse(args.ElementAtOrDefault(1), out _maxShowId))
+				throw new ArgumentException("Second argument is the maximum show id");
+
+			var thirdArg = args.ElementAtOrDefault(2);
+			if (thirdArg == null)
+				throw new ArgumentException("Third argument is the target download directory");
+			_saveDirectory = new DirectoryInfo(thirdArg);
+			if (!_saveDirectory.Exists)
+				throw new ArgumentException("Save directory does not exist");
+		}
+
+		private static IEnumerable<Show> GetShows()
+		{
+			var xDocument = LoadXmlFeed();
+
+			return from item in xDocument.Descendants("item")
+			       let mp3LinkElem = item.Descendants(FeedBurnerNamespace + "origEnclosureLink").Single()
+			       select new Show
+			              	{
+			              		Id = int.Parse(ShowIdRegex.Matches(mp3LinkElem.Value)[0].Groups[1].ToString()),
+			              		Mp3Uri = new Uri(mp3LinkElem.Value)
+			              	};
+		}
+
+		private static XDocument LoadXmlFeed()
+		{
+			return XDocument.Load(Mp3Feed.AbsoluteUri);
 		}
 	}
 }
---
+++ HanselDownloader/Show.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HanselDownloader
+{
+	internal class Show
+	{
+		public int Id { get; set; }
+
+		public Uri Mp3Uri { get; set; }
+	}
+}
